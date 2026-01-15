abf5f7710ff3983df3b989c225d99d7cd7e289f2
Andrzej Krawczyk <akr@humandialog.com.pl>
2026-01-15 07:23:29 +0100

    kanban-paper sm:table

--- src/lib/paper.table.svelte
+++ src/lib/paper.table.svelte
@@ -2,7 +2,7 @@
     let additionalClass = $$props.class ?? ''
 </script>
 
-<section class="    paper table
+<section class="    kanban-paper sm:table
                     prose prose-base prose-zinc dark:prose-invert prose-a:no-underline
 
                     max-w-none sm:min-h-[32rem]
