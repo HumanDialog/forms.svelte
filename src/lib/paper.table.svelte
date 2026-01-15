6d7e2a87cd33d4c423d792c27577767c18c5ced4
Andrzej Krawczyk <akr@humandialog.com.pl>
2026-01-15 09:56:05 +0100

    KANBAN SCROLL FIX

--- src/lib/paper.table.svelte
+++ src/lib/paper.table.svelte
@@ -6,7 +6,7 @@
                     prose prose-base prose-zinc dark:prose-invert prose-a:no-underline
 
                     !max-w-none sm:min-h-[32rem]
-
+                    
                     sm:pt-8 sm:xpx-4
                     md:xpx-10 md:mx-2
                 ">
